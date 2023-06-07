ea72f43eb3f8ee30f10e6a37f4d3543b06bc8bea
100vision <30432527+100vision@users.noreply.github.com>
2023-06-07 08:50:28 +0000

    Deploying to gh-pages from @ 100vision/100vision.github.io@a168f3d0755b91f6218758153369fe1f5aaca51a 🚀

---
+++ assets/post35_几个にて比较（区分）.html-ffca0618.js
@@ -0,0 +1,11 @@
+import{_ as e,W as i,X as a,Y as n}from"./framework-2e6688e7.js";const d={},t=n(`<h3 id="「にとって」" tabindex="-1"><a class="header-anchor" href="#「にとって」" aria-hidden="true">#</a> 「にとって」</h3><p>翻译：对xxx（人）来说，站在...角度。小句常有用来评价词语。 对应英文里有 &quot;for&quot;</p><h3 id="「によって」" tabindex="-1"><a class="header-anchor" href="#「によって」" aria-hidden="true">#</a> 「によって」</h3><p>有两种用法，</p><ul><li><p>表原因。这时和 「で」可以替换使用。可对应英文的&quot;by&quot;,&quot;because&quot;。翻译：&quot;因为/由于....&quot;</p></li><li><p>表情况差异。类似英语 It depends 和 depending。翻译“根据...&quot; (不同/差异)，常搭配「違う」</p></li></ul><h3 id="「について」" tabindex="-1"><a class="header-anchor" href="#「について」" aria-hidden="true">#</a> 「について」</h3><ul><li>表关联和联系。翻译: &quot;关于...&quot;，有一个书面用法是「に関する」，可以对应英文介词 &quot;about&quot;</li></ul><h3 id="「に対して」、-「に対する」" tabindex="-1"><a class="header-anchor" href="#「に対して」、-「に対する」" aria-hidden="true">#</a> 「に対して」、 「に対する」</h3><p></p><ul><li>表针对。翻译：&quot;针对...&quot;。 可对应英文介词 &quot;torwards&quot;</li></ul><p>小句没有动词谓语用「に対する」，小句有动词谓语用「に対して」</p><p>另外「に対する」后面可以直接名词，翻译：针对xxx的</p><p><code>男に対する規則は酷いです</code></p><h3 id="例" tabindex="-1"><a class="header-anchor" href="#例" aria-hidden="true">#</a> 例</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>彼にとって大切な話
+An important topic for him.
+
+彼について知っていること
+Matters we know regarding him.
+
+彼に対して意見をいう。
+speak my opinion agrainst him.
+
+台風によって大きな被害を受けた。（by）
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,15),r=[t];function s(l,o){return i(),a("div",null,r)}const c=e(d,[["render",s],["__file","post35_几个にて比较（区分）.html.vue"]]);export{c as default};
